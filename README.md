e30e64b58fc67ce1c0f52dcdb3704d3b78053f6a
Egor Danchenkov <icepnyx@gmail.com>
2017-04-30 14:37:37 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# Twitterhyno
+
+Twitterhyno is Automation app for Twitter 
+
+## Install
+
+```
+npm i -g typescript webpack
+npm link typescript
+npm i
+```
+
+## Run
+
+```
+npm run dev
+```
+
+## Build
+
+```
+npm run build
+```
---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+module.exports = {
+    entry: "./src/index.ts",
+    output: {
+        filename: "bundle.js",
+        path: __dirname + "/build"
+    },
+    // Awesome typescript loader stores errors in webpack stats
+    // Print full error
+    stats: {
+        colors: true,
+        modules: true,
+        reasons: true,
+        errorDetails: true
+    },
+
+    // Enable sourcemaps for debugging webpack's output.
+    devtool: "source-map",
+
+    resolve: {
+        // Add '.ts' and '.tsx' as resolvable extensions.
+        extensions: [".ts", ".tsx", ".js", ".json"]
+    },
+
+    module: {
+        rules: [
+            // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
+            {
+                test: /\.tsx?$/,
+                loader: "awesome-typescript-loader"
+            },
+
+            // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+            {
+                enforce: "pre",
+                test: /\.js$/,
+                loader: "source-map-loader"
+            },
+            {
+                test: /\.ts$/,
+                enforce: 'pre',
+                loader: 'tslint-loader',
+                options: { /* Loader options go here */ }
+            }
+        ]
+    },
+};
