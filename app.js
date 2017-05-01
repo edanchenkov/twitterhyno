4e2432df98ec51c4ff9af8c392019290b355400a
Egor Danchenkov <icepnyx@gmail.com>
2017-05-01 22:39:20 +0300

    Refactor project, add express app to call Twitter API

---
+++ app.js
@@ -0,0 +1,58 @@
+var express = require('express');
+var request = require('request');
+var qs = require('querystring');
+
+var app = express();
+
+var config = require('./config');
+
+/* Serve index html, css and js */
+app.use('/', express.static('build'));
+
+app.get('/auth/request_token', function (req, res) {
+    var oauth = {
+        callback : config.server.protocol + '://' + config.server.host + '/auth/callback',
+        consumer_key : config.twitterApp.consumerKey,
+        consumer_secret : config.twitterApp.consumerSecret
+    };
+
+    request.post({
+        url : config.requestTokenUrl, oauth : oauth
+    }, function (error, response, body) {
+        var data = qs.parse(body);
+        res.status(302).send(data);
+    });
+});
+
+app.get('/auth/callback', function (req, res) {
+    var oauth = {
+        verifier : req.query.oauth_verifier,
+        token : req.query.oauth_token
+    };
+
+    request.post({
+        url : config.accessTokenUrl, oauth : oauth
+    }, function (error, response, body) {
+        var data = qs.parse(body);
+        request.get({
+            url : config.verifyCredentials,
+            oauth : {
+                token_secret : data.oauth_token_secret,
+                token : data.oauth_token,
+                consumer_key : config.twitterApp.consumerKey,
+                consumer_secret : config.twitterApp.consumerSecret
+            }
+        }, function (error, response, body) {
+            var data = qs.parse(body);
+            console.log(data)
+            res.send(response)
+
+        })
+    });
+
+
+});
+
+app.listen(config.server.port, function () {
+    console.log('Example app listening on port', config.server.port);
+});
